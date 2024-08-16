dd4f608d5193dd37af6cc064a59f7eb86c50a885
ShivaRK <shivarkcodes@gmail.com>
2024-08-16 21:07:35 +0530

    fix:blob problem

--- src/app/components/Blob.js
+++ src/app/components/Blob.js
@@ -1,47 +1,27 @@
 "use client"
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import './Blob.css';
 
 const Blob = () => {
-    const [curX, setCurX] = useState(0);
-    const [curY, setCurY] = useState(0);
-    const targetPosition = useRef({ x: 0, y: 0 });
     const interBubbleRef = useRef(null);
 
     useEffect(() => {
         const handleMouseMove = (event) => {
-            targetPosition.current = { x: event.clientX, y: event.clientY };
-        };
-
-        const move = () => {
-            setCurX((prevX) => prevX + (targetPosition.current.x - prevX) / 20);
-            setCurY((prevY) => prevY + (targetPosition.current.y - prevY) / 20);
-
             if (interBubbleRef.current) {
-                interBubbleRef.current.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
+                const x = event.clientX + window.scrollX;
+                const y = event.clientY + window.scrollY;
+                interBubbleRef.current.style.transform = `translate(${x}px, ${y}px)`;
             }
-
-            requestAnimationFrame(move);
         };
 
         window.addEventListener('mousemove', handleMouseMove);
-        move();
 
         return () => {
             window.removeEventListener('mousemove', handleMouseMove);
         };
-    }, [curX, curY]);
+    }, []);
 
-    return <div class="gradient-bg fixed z-[1]">
-        <div class="gradients-container fixed z-[1]">
-            {/* <div class="g1"></div> */}
-            {/* <div class="g2"></div> */}
-            {/* <div class="g3"></div> */}
-            {/* <div class="g4"></div> */}
-            {/* <div class="g5"></div> */}
-            <div className='z-[1]' class="interactive" ref={interBubbleRef}></div>
-        </div>
-    </div>
+    return <div className="interactive" ref={interBubbleRef}></div>;
 };
 
 export default Blob;
